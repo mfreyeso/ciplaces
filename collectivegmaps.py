0670f4c05ddd817693b67d1cc43c26eb0f6bbfd5
Mario Reyes Ojeda <mfreyesojeda@gmail.com>
2017-11-20 13:28:08 -0500

    Add form to load isolate credentials

--- collectivegmaps.py
+++ collectivegmaps.py
@@ -12,21 +12,21 @@ class CollectivePlaces(object):
         return self.client.place(placeid, language=self.language)
 
     def search_places_nearby(self, location, keyword,
-                             min_price, max_price, type_rank):
+                             min_price=0, max_price=4, type_rank):
 
         return self.client.places_nearby(location, keyword=keyword,
                                          language=self.language,
-                                         min_price=min_price,
+                                         min_price=min_price=0,
                                          max_price=max_price,
                                          rank_by=type_rank)
 
-    def search_places_radar(self, location, keyword, min_price, max_price):
+    def search_places_radar(self, location, keyword, min_price=0, max_price=4):
         return self.client.places_radar(location, self.radius,
                                         keyword=keyword,
-                                        min_price=min_price,
+                                        min_price=min_price=0,
                                         max_price=max_price)
 
-    def search_places_by_text(self, textplace, location, min_price, max_price):
+    def search_places_by_text(self, textplace, location, min_price=0, max_price=4):
         return self.client.places(textplace, location=location,
                                   radius=self.radius,
                                   language=self.language,
@@ -40,7 +40,5 @@ class CollectiveLocations(object):
         self.client = googlemaps.Client(key=apikey)
         self.language = 'en'
 
-
     def get_geolocation(self, textplace):
         return self.client.geocode(textplace)
-
--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 import collectivegmaps
-API_KEY = 'AIzaSyBL70Wp4zJmAlldLEjga2Ya7RwJSk_3Q6s'
-GAPI_KEY = 'AIzaSyAFXycMh-SralJrW1brFAnx6VYrxldCzqw'
+import json
+import configparser
 
-locations = collectivegmaps.CollectiveLocations(GAPI_KEY)
+class HandlerData(object):
 
-geocode = locations.get_geolocation('Temple Pl')
-print(geocode)
+    def __init__(self):
+        self.config = configparser.ConfigParser()
+
+    def loadconfig(self):
+        self.config.read('example.ini')
+
+        API_KEY_LOCATIONS = self.config['credentials']['geolocation']
+        API_KEY_PLACES = self.config['credentials']['places']
+
+        self.clocations = collectivegmaps.CollectiveLocations(
+            API_KEY_LOCATIONS)
+        self.cplaces = collectivegmaps.CollectivePlaces(API_KEY_PLACES)
+
+
+def main():
+    handler = HandlerData()
+    handler.loadconfig()
+
+    geocode = handler.clocations.get_geolocation('Temple Pl')
+    print(geocode)
+
+
+
+
+if __name__ == '__main__':
+    main()
